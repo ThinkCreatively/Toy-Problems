08f59d105297938e9e49816ca0f5d787269883f0
James Moore <jameswmoore99@gmail.com>
2021-07-26 11:07:20 -0600

    Added many toy problems

--- equivalentFoldedSums/equivalentFoldedSums.js
+++ equivalentFoldedSums/equivalentFoldedSums.js
@@ -1,3 +1,10 @@
+/*
+You are given a list of integers nums of even length. Consider an operation where you pick any number in nums
+and update it with a value between [1, max(nums)].
+
+Return the minimum number of operations required such that for every i, nums[i] + nums[n - 1 - i] equals to the same number.
+*/
+
 const equivalentFoldedSums = (array) => {
   let outerSum = array[0] + array[array.length - 1];
   let innerSum = array[1] + array[array.length - 2];

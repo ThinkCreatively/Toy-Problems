2ef7514471cee8df99b96be4b7b3e488072f619e
James Moore <jameswmoore99@gmail.com>
2021-08-04 11:58:04 -0600

    Passes all tests but one

--- numberOfOneBits/numberOfOneBits.js
+++ numberOfOneBits/numberOfOneBits.js
@@ -4,14 +4,22 @@
 */
 
 function hammingWeight(n) {
-  n = n.toString();
-  if (n.length !== 32) {
-    return 0;
+  // Create result (number of 1 bits)
+  let result = 0;
+  console.log(n)
+  // Change integer to String
+  n = (n >>> 0).toString(2);
+  console.log('after conversion: ', n)
+  // Iterate through n
+  for (let i = 0; i < n.length; i++) {
+    // Count all 1's
+    if (n[i] === '1') {
+      result++;
+    }
   }
+  return result;
 }
 
 console.log(hammingWeight(00000000000000000000000000001011)); // return 3
 console.log(hammingWeight(00000000000000000000000010000000)); // return 1
 console.log(hammingWeight(11111111111111111111111111111101)); // return 31
-console.log(hammingWeight(000)); // return 0 since length doesnt equal 32
-console.log(hammingWeight(1)); // return 0 since length doesn't equal 32

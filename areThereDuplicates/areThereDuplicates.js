27eebcae9817ba857d8711cacbc1a7b0120a45d6
James Moore <jameswmoore99@gmail.com>
2021-08-18 14:07:29 -0600

    Working through multiple pointers approach for areThereDuplicates

--- areThereDuplicates/areThereDuplicates.js
+++ areThereDuplicates/areThereDuplicates.js
@@ -2,14 +2,29 @@
 // EX: (1, 2, 2) = true while (1, 2, 3) = false
 // In this problem I will be doing it first with a frequency counter approach then a multiple pointers approach
 
+// Frequency counter approach
+
+// function areThereDuplicates(...args) {
+//   let frequency = {};
+//   for (let i = 0; i < args.length; i++) {
+//     let currentVal = args[i];
+//     if (frequency[currentVal] === 1) {
+//       return true;
+//     }
+//     frequency[currentVal] ? frequency[currentVal]++ : frequency[currentVal] = 1;
+//   }
+//   return false;
+// };
+
+// Multiple Pointers approach
+
 function areThereDuplicates(...args) {
-  let frequency = {};
-  for (let i = 0; i < args.length; i++) {
-    let currentVal = args[i];
-    if (frequency[currentVal] === 1) {
+  let firstPointer = args[0];
+  for (let i = 1; i < args.length; i++) {
+    let secondPointer = args[i];
+    if (firstPointer === secondPointer) {
       return true;
     }
-    frequency[currentVal] ? frequency[currentVal]++ : frequency[currentVal] = 1;
   }
   return false;
 };

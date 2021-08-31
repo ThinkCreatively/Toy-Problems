ff270fdc4cb40af9e0221f4a4793cf941f933915
James Moore <jameswmoore99@gmail.com>
2021-08-30 18:13:01 -0600

    Solved reverse string with recursive function approach

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,19 +1,32 @@
 // Problem: Using recursion, reverse string
 
-function reverse(str) {
-  let result = '';
-  function getLetter(str) {
-    if (str === '') return
-    str = str.split('');
-    result += str[str.length - 1];
-    str.pop();
-    str = str.join('');
-    getLetter(str);
-  }
-  getLetter(str);
-  return result;
+// function reverse(str) {
+//   let result = '';
+//   function getLetter(str) {
+//     if (str === '') return
+//     str = str.split('');
+//     result += str[str.length - 1];
+//     str.pop();
+//     str = str.join('');
+//     getLetter(str);
+//   }
+//   getLetter(str);
+//   return result;
+// }
+
+function reverse(str, result) {
+  if (str.length === 1) {
+    result += str;
+    return result;
+  };
+  str = str.split('');
+  result += str[str.length - 1];
+  str.pop();
+  str = str.join('');
+  return reverse(str, result);
 }
 
-console.log(reverse('James')); // 'semaJ'
-console.log(reverse('Hi')); // 'iH'
-console.log(reverse('awesome')); // 'emosewa'
+
+console.log(reverse('James', '')); // 'semaJ'
+console.log(reverse('Hi', '')); // 'iH'
+console.log(reverse('awesome', '')); // 'emosewa'

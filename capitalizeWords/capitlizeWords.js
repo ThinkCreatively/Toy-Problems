dd34b6bc8e4880e449361aa7ae92bedf1a085d8b
James Moore <jameswmoore99@gmail.com>
2021-08-31 16:06:59 -0600

    Solved capitlizeWords with recursive function approach

--- capitalizeWords/capitlizeWords.js
+++ capitalizeWords/capitlizeWords.js
@@ -2,24 +2,35 @@
 
 // Helper function
 
-function capitalizeWords(array) {
-  let result = [];
-  function capitalize(array) {
-    for(word of array) {
-      word = word.toUpperCase();
-      result.push(word);
-      array.shift();
-      capitalize(array)
-    }
-  }
-  capitalize(array);
-  return result;
-}
+// function capitalizeWords(array) {
+//   let result = [];
+//   function capitalize(array) {
+//     for(word of array) {
+//       word = word.toUpperCase();
+//       result.push(word);
+//       array.shift();
+//       capitalize(array)
+//     }
+//   }
+//   capitalize(array);
+//   return result;
+// }
 
 // Recursive Function
 
-// function capitalizeWords() {
-
-// }
+function capitalizeWords(array) {
+  let result = [];
+  if (array.length === 1) {
+    lastWord = array[0].toUpperCase();
+    result.push(lastWord);
+    return result;
+  }
+  for(word of array) {
+    word = word.toUpperCase();
+    result.push(word);
+    array.shift();
+    return result.concat(capitalizeWords(array));
+  }
+}
 
 console.log(capitalizeWords(['i', 'am', 'practicing', 'recursion'])); // ['I', 'AM', 'PRACTICING', 'RECURSION']

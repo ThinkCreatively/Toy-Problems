be9884669b2dd338fa2b3b1e001337775d2a6d6d
James Moore <james3.moore@dish.com>
2023-10-11 10:56:42 -0600

    ~ Cleaned up errors, added test cases

--- 2023/containsDuplicate/containsDuplicate.py
+++ 2023/containsDuplicate/containsDuplicate.py
@@ -1,8 +1,11 @@
+from typing import List
 # PROMPT:
 # Given an integer array nums, return true if any value appears at least twice in the array,
 # and return false if every element is distinct.
 
+def containsDuplicate(nums: List[int]) -> bool:
+    return len(set(nums)) != len(nums)
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) == len(nums)
+print('Result of contains duplicates with [1,2,3,1], should return True: ', containsDuplicate([1,2,3,1]))
+print('Result of contains duplicates with [1,2,3,4], should return False: ', containsDuplicate([1,2,3,4]))
+print('Result of contains duplicates with [1,1,1,3,3,4,3,2,4,2], should return True: ', containsDuplicate([1,1,1,3,3,4,3,2,4,2]))

92ff688c7342642648ca78c7433ea8a633d760ed
James Moore <jameswmoore99@gmail.com>
2024-02-06 12:17:40 -0700

    ~ Added my brute force solution

--- 2024/groupAnagrams.ts
+++ 2024/groupAnagrams.ts
@@ -1,4 +1,4 @@
-const groupAnagrams = (strs: string[]): string[][] => {
+const chatGroupAnagrams = (strs: string[]): string[][] => {
   if (strs.length <= 1) return [strs];
 
   let anagrams = {};
@@ -12,4 +12,34 @@ const groupAnagrams = (strs: string[]): string[][] => {
   }
 
   return Object.values(anagrams);
+};
+
+// alternate Brute for solution
+const myGroupAnagrams = (strs: string[]): string[][] => {
+  if (strs.length <= 1) return [strs];
+
+  let copy = strs;
+  let result: string[][] = [];
+  let group: string[] = [];
+  let temp;
+
+  for (let i = 0; i < copy.length; i++) {
+    temp = copy[i].split("").sort();
+    group.push(copy[i]);
+
+    for (let j = i + 1; j < copy.length; j++) {
+      let splitted = copy[j].split("").sort();
+      if (temp.join("") === splitted.join("")) {
+        group.push(copy[j]);
+        copy.splice(j, 1);
+        j--;
+      }
+    }
+
+    copy.splice(i, 1);
+    result.push(group);
+    group = [];
+    i--;
+  }
+  return result;
 };

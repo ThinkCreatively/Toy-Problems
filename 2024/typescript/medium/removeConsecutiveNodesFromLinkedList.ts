2f20abf480145b4dea85e84f69e65d1c6b096945
James Moore <jameswmoore99@gmail.com>
2024-03-12 12:38:40 -0600

    ~ Attempted removeConsecutiveNodesFromLinkedLists

---
+++ 2024/typescript/medium/removeConsecutiveNodesFromLinkedList.ts
@@ -0,0 +1,64 @@
+//TODO: ATTEMPTED NOT COMPLETE
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function removeZeroSumSublists(head: ListNode | null): ListNode | null {
+  let values: number[] = [];
+
+  const getVals = (node: ListNode | null) => {
+    if (!node) return;
+
+    values.push(node.val);
+
+    getVals(node.next);
+  };
+
+  getVals(head);
+
+  let prefixSums = [];
+  let sum = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    sum += values[i];
+    prefixSums.push(sum);
+  }
+
+  let deleted = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] + values[i + 1] === 0) {
+      values.splice(i, 2);
+      deleted++;
+      i = -1;
+    }
+  }
+
+  if (!values.length) return null;
+
+  if (!deleted) {
+  }
+
+  let list = new ListNode(values[0], null);
+  values.shift();
+
+  const build = (node: ListNode | null): ListNode => {
+    if (!node.next && values.length > 0) {
+      node.next = new ListNode(values[0], null);
+      values.shift();
+      build(node.next);
+    } else {
+      return;
+    }
+  };
+
+  build(list);
+
+  return head;
+}

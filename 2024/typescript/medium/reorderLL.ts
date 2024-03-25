07b07ec6b6213bdcba3643d04a5b9eb01884873a
James Moore <jameswmoore99@gmail.com>
2024-03-25 11:15:55 -0600

    ~ Finished reorderLL

---
+++ 2024/typescript/medium/reorderLL.ts
@@ -0,0 +1,34 @@
+function reorderList(head: ListNode | null): void {
+  let values: number[] = [];
+  let node = head;
+
+  while (node) {
+    values.push(node.val);
+    node = node.next;
+  }
+
+  let order: number[] = [];
+  let p1 = 0;
+  let p2 = values.length - 1;
+
+  while (p1 < p2) {
+    order.push(values[p1], values[p2]);
+    p1++;
+    p2--;
+  }
+
+  if (values.length % 2 !== 0) {
+    order.push(values[p1]);
+  }
+
+  let count = 0;
+
+  const change = (node: ListNode | null) => {
+    if (!node) return;
+    node.val = order[count];
+    count++;
+    change(node.next);
+  };
+
+  change(head);
+}

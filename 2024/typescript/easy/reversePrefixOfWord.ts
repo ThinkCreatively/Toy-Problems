19f3bfa774e5a0839c52dd6f51e37b9790744bf3
James Moore <jameswmoore99@gmail.com>
2024-05-01 10:10:18 -0600

    ~ refactored reversePrefixOfWord

--- 2024/typescript/easy/reversePrefixOfWord.ts
+++ 2024/typescript/easy/reversePrefixOfWord.ts
@@ -1,24 +1,41 @@
 function reversePrefix(word: string, ch: string): string {
   let result: string[] = [];
   let splitted = word.split("");
-  let idx;
+  let idx = splitted.indexOf(ch);
 
   if (!splitted.includes(ch)) {
     return word;
   }
 
-  for (let i = 0; i < splitted.length; i++) {
-    if (word[i] !== ch) {
-      result.push(word[i]);
-    } else {
-      result.push(word[i]);
-      idx = i;
-      break;
-    }
-  }
-
-  result = result.reverse();
+  result = result.concat(splitted.slice(0, idx + 1)).reverse();
   result = result.concat(splitted.slice(idx + 1));
 
   return result.join("");
 }
+
+
+// First iteration
+// function reversePrefix(word: string, ch: string): string {
+//   let result: string[] = [];
+//   let splitted = word.split("");
+//   let idx;
+
+//   if (!splitted.includes(ch)) {
+//     return word;
+//   }
+
+//   for (let i = 0; i < splitted.length; i++) {
+//     if (word[i] !== ch) {
+//       result.push(word[i]);
+//     } else {
+//       result.push(word[i]);
+//       idx = i;
+//       break;
+//     }
+//   }
+
+//   result = result.reverse();
+//   result = result.concat(splitted.slice(idx + 1));
+
+//   return result.join("");
+// }

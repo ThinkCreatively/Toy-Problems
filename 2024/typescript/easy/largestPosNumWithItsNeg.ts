c3b2f0c07650b765cd999c104f1081126f2008a2
James Moore <jameswmoore99@gmail.com>
2024-05-02 09:54:53 -0600

    ~ Refactored largestPositiveWithItsNegative

--- 2024/typescript/easy/largestPosNumWithItsNeg.ts
+++ 2024/typescript/easy/largestPosNumWithItsNeg.ts
@@ -1,17 +1,31 @@
+// Second iteration
 function findMaxK(nums: number[]): number {
   let sorted = nums.sort((a, b) => a - b);
-  let positives: number[] = [];
-  let negatives: number[] = [];
 
-  for (let num of sorted) {
-    num < 0 ? negatives.push(num) : positives.push(num);
-  }
-
-  for (let i = positives.length - 1; i > -1; i--) {
-    if (negatives.includes(positives[i] * -1)) {
-      return positives[i];
+  for (let i = sorted.length - 1; i > -1; i--) {
+    if (sorted.includes(sorted[i] * -1)) {
+      return sorted[i];
     }
   }
 
   return -1;
 }
+
+// First iteration
+// function findMaxK(nums: number[]): number {
+//   let sorted = nums.sort((a, b) => a - b);
+//   let positives: number[] = [];
+//   let negatives: number[] = [];
+
+//   for (let num of sorted) {
+//     num < 0 ? negatives.push(num) : positives.push(num);
+//   }
+
+//   for (let i = positives.length - 1; i > -1; i--) {
+//     if (negatives.includes(positives[i] * -1)) {
+//       return positives[i];
+//     }
+//   }
+
+//   return -1;
+// }

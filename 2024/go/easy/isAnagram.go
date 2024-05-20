b8f16a586d3945433abfc28b8b28f0b60dfc8fbf
James Moore <jameswmoore99@gmail.com>
2024-05-20 11:48:52 -0600

    ~ Finished isAnagram in go

---
+++ 2024/go/easy/isAnagram.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"reflect"
+	"unicode"
+)
+
+func isAnagram(s string, t string) bool {
+	sRunes := map[rune]int{}
+	tRunes := map[rune]int{}
+
+	for _, v := range s {
+		if unicode.IsSpace(v) {
+			continue
+		}
+		sRunes[v] += 1
+	}
+
+	for _, v := range t {
+		if unicode.IsSpace(v) {
+			continue
+		}
+		tRunes[v] += 1
+	}
+
+	return reflect.DeepEqual(sRunes, tRunes)
+}

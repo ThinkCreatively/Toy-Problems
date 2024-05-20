ba9b779d244096bd6a34f05e5c092cbeca0fe0d5
James Moore <jameswmoore99@gmail.com>
2024-05-20 13:08:04 -0600

    ~ Finihsed twoSum using ranges

--- 2024/go/easy/twoSum.go
+++ 2024/go/easy/twoSum.go
@@ -2,13 +2,25 @@ package main
 
 //lint:ignore U1000 Ignore unused function
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
+	for i, iv := range nums {
+		for j, jv := range nums[i+1:] {
+			if iv+jv == target {
+				return []int{i, i + j + 1}
 			}
 		}
 	}
 
 	return []int{0, 0}
 }
+
+// func twoSum(nums []int, target int) []int {
+// 	for i := 0; i < len(nums); i++ {
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i]+nums[j] == target {
+// 				return []int{i, j}
+// 			}
+// 		}
+// 	}
+
+// 	return []int{0, 0}
+// }

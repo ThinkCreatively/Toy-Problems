39e41ea1137baa85d438af4a868720f806754474
James Moore <jameswmoore99@gmail.com>
2024-05-20 12:59:48 -0600

    ~ finished twoSum in golang

---
+++ 2024/go/easy/twoSum.go
@@ -0,0 +1,14 @@
+package main
+
+//lint:ignore U1000 Ignore unused function
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{0, 0}
+}

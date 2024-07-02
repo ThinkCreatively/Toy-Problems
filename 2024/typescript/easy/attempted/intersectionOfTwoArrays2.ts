8c89c7520480afd38cd5be3fd67e2ad8166a3324
James Moore <jameswmoore99@gmail.com>
2024-07-02 12:04:49 -0600

    ~ 2nd attempt logged in for intersectionofTwoArrays2

--- 2024/typescript/easy/attempted/intersectionOfTwoArrays2.ts
+++ 2024/typescript/easy/attempted/intersectionOfTwoArrays2.ts
@@ -33,3 +33,46 @@ function intersect(nums1: number[], nums2: number[]): number[] {
 
   return result;
 }
+
+// Second attempt
+
+// function intersect(nums1: number[], nums2: number[]): number[] {
+//   if (nums1.length === nums2.length) {
+//     for (let i = 0; i < nums1.length; i++) {
+//       if (nums1[i] === nums2[i]) {
+//         if (nums1[i + 1] === nums2[i + 1] || nums1[i - 1] === nums2[i - 1]) {
+//           return nums1;
+//         }
+//       }
+//     }
+//   }
+
+//   let smallest;
+//   let largest;
+
+//   if (nums1.length > nums2.length) {
+//     smallest = nums1;
+//     largest = nums2;
+//   } else {
+//     smallest = nums2;
+//     largest = nums1;
+//   }
+
+//   for (let i = 0; i < largest.length; i++) {
+//     if (largest[i] === smallest[0]) {
+//       if (largest[i + 1] === smallest[1]) {
+//         return [smallest[0], smallest[1]];
+//       }
+//     }
+
+//     if (largest[i] === smallest[1]) {
+//       if (largest[i + 1] === smallest[2]) {
+//         return [smallest[1], smallest[2]];
+//       }
+
+//       if (largest[i - 1] === smallest[0]) {
+//         return [smallest[1], smallest[0]];
+//       }
+//     }
+//   }
+// }

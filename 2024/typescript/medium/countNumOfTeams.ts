fe837d2f4263543d8190fd129b252b7aa0d2f650
James Moore <jameswmoore99@gmail.com>
2024-07-29 11:44:48 -0600

    ~ Included alternate solution

--- 2024/typescript/medium/countNumOfTeams.ts
+++ 2024/typescript/medium/countNumOfTeams.ts
@@ -26,3 +26,38 @@ function numTeams(rating: number[]): number {
 
   return numOfTeams;
 }
+
+// Chat Solution:
+// function numTeams(rating: number[]): number {
+//   let n = rating.length;
+//   let count = 0;
+
+//   for (let j = 0; j < n; j++) {
+//       let leftSmaller = 0;
+//       let leftLarger = 0;
+//       let rightSmaller = 0;
+//       let rightLarger = 0;
+
+//       for (let i = 0; i < j; i++) {
+//           if (rating[i] < rating[j]) {
+//               leftSmaller++;
+//           }
+//           if (rating[i] > rating[j]) {
+//               leftLarger++;
+//           }
+//       }
+
+//       for (let k = j + 1; k < n; k++) {
+//           if (rating[k] < rating[j]) {
+//               rightSmaller++;
+//           }
+//           if (rating[k] > rating[j]) {
+//               rightLarger++;
+//           }
+//       }
+
+//       count += leftSmaller * rightLarger + leftLarger * rightSmaller;
+//   }
+
+//   return count;
+// }

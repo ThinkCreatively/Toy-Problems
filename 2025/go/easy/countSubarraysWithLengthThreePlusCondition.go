e32bf1def8de2056d1f70dff14eb9e07153c3b1c
ThinkCreatively <jameswmoore99@gmail.com>
2025-04-28 09:45:33 -0600

    added my wip solution

--- 2025/go/easy/countSubarraysWithLengthThreePlusCondition.go
+++ 2025/go/easy/countSubarraysWithLengthThreePlusCondition.go
@@ -1,6 +1,47 @@
 package easy
 
-// Chat answer
+// My WIP Solution
+func countSubarrays(nums []int) int {
+	if len(nums) == 3 && nums[0]+nums[2] != nums[1]/2 {
+		return 0
+	}
+
+	subArrs := [][]int{}
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			for k := j + 1; k < len(nums); k++ {
+				candidate := []int{nums[i], nums[j], nums[k]}
+				if nums[i]+nums[k] == nums[j]/2 && !contains(subArrs, candidate) {
+					subArrs = append(subArrs, []int{nums[i], nums[j], nums[k]})
+				}
+			}
+		}
+	}
+
+	return len(subArrs)
+}
+
+func contains(subArrs [][]int, candidate []int) bool {
+	for _, arr := range subArrs {
+		if len(arr) != len(candidate) {
+			continue
+		}
+		matched := true
+		for i := range arr {
+			if arr[i] != candidate[i] {
+				matched = false
+				break
+			}
+		}
+		if matched {
+			return true
+		}
+	}
+	return false
+}
+
+// Chats answer
 func countValidSubarrays(nums []int) int {
 	count := 0
 
